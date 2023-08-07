011228b4671fa6a58b899ba12cbc5c1ae1dd46fb
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-08-08 01:40:41 +0900

    Added Flask first sample app

---
+++ pheasant/flask/app.py
@@ -0,0 +1,33 @@
+'''
+参考
+https://code.visualstudio.com/docs/python/tutorial-flask
+'''
+
+from flask import Flask
+import re
+from datetime import datetime
+import locale
+
+app = Flask(__name__)
+locale.setlocale(locale.LC_TIME, 'ja_JP.UTF-8')
+
+@app.route("/pheasant/")
+def home():
+    return "こんにちは, Flask♪"
+
+@app.route("/pheasant/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%a曜日, %B %d日, %Y at %X")
+
+    #危険な文字はフィルタリングで弾かれる。
+    match_object = re.match("[a-zA-Z]+", name)
+
+    #今のところマルチバイト文字はマッチしない。
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "どこかの誰か"
+
+    content = "お世話になっております, " + clean_name + "! 今は" + formatted_now
+    return content
