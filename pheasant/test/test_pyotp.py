351c4d0cea3827eeeb2ae4320f35ab11248dd34f
moguonyako <moguonyanko@gmail.com>
2025-09-28 01:01:56 +0900

    Added PyOTP test

--- pheasant/test/test_pyotp.py
+++ pheasant/test/test_pyotp.py
@@ -7,10 +7,19 @@ https://pypi.org/project/pyotp/
 import pyotp
 import time
 
-totp = pyotp.TOTP('base32secret3232')
-totp.now() # => '492039'
+def test_verify_pyotp():
+  """
+  PyOTPで作成したワンタイムパスワードの有効期限に関する振る舞いをテストします。
+  """
+  secret_key = pyotp.random_base32()
+  # テストのため1秒でパスワードの有効期限が切れるようにする。規定値は30秒である。
+  totp = pyotp.TOTP(secret_key, interval=1) 
+  password = totp.now()
 
-# OTP verified for current time
-totp.verify('492039') # => True
-time.sleep(30)
-totp.verify('492039') # => False
+  # 有効期限が切れていないことのテスト
+  assert totp.verify(password)
+  time.sleep(1)
+  
+  # 有効期限が切れていることのテスト
+  assert not totp.verify(password)
+
